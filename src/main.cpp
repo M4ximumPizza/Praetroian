aab7b6d96a9738903e06d8a796051e851fd67a00
M4ximumPizza <MaximumUltimateDestroyer@gmail.com>
2024-02-27 10:14:46 -0800

    Made it so it directly reads Java files

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include <fstream>
 #include "lexer.h"
 #include "parser.h"
 #include "terminal.h"
 
 int main() {
-    std::string sourceCode = "public class Main {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println(\"Hello, World!\");\n\t}\n}";
+    // Open the file
+    std::ifstream file("javatest/Main.java");
+    if (!file.is_open()) {
+        std::cerr << "Error opening file." << std::endl;
+        return 1;
+    }
 
+    // Read the content of the file into a string
+    std::string sourceCode((std::istreambuf_iterator<char>(file)),
+                       (std::istreambuf_iterator<char>()));
+
+    // Close the file
+    file.close();
+
+    // Process the source code
     Lexer lexer(sourceCode);
     Parser parser(lexer);
     parser.parse();
